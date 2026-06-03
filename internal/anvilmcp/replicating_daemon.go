44f2d1d43943a055f49a9fc812262678f7c019e7
HardcoreMonk <smtlkbs0312@gmail.com>
2026-06-03 16:02:51 +0900

    feat: delegate flock creation to router

--- internal/anvilmcp/replicating_daemon.go
+++ internal/anvilmcp/replicating_daemon.go
@@ -7,14 +7,23 @@ import (
 
 type ReplicatingDaemon struct {
 	Daemon
-	replicator snapshotReplicator
+	replicator  snapshotReplicator
+	flockRouter flockCreator
+}
+
+type flockCreator interface {
+	CreateFlock(context.Context, FlockCreateRequest) (*FlockCreateResponse, error)
 }
 
 func NewReplicatingDaemon(base Daemon, replicator snapshotReplicator) *ReplicatingDaemon {
-	return &ReplicatingDaemon{
+	daemon := &ReplicatingDaemon{
 		Daemon:     base,
 		replicator: replicator,
 	}
+	if router, ok := replicator.(flockCreator); ok {
+		daemon.flockRouter = router
+	}
+	return daemon
 }
 
 func (d *ReplicatingDaemon) ReplicateSnapshot(ctx context.Context, req SnapshotReplicationRequest) (*SnapshotReplicationResponse, error) {
@@ -22,4 +31,11 @@ func (d *ReplicatingDaemon) ReplicateSnapshot(ctx context.Context, req SnapshotR
 		return nil, fmt.Errorf("snapshot replicator is nil")
 	}
 	return d.replicator.ReplicateSnapshot(ctx, req)
+}
+
+func (d *ReplicatingDaemon) CreateFlock(ctx context.Context, req FlockCreateRequest) (*FlockCreateResponse, error) {
+	if d != nil && d.flockRouter != nil {
+		return d.flockRouter.CreateFlock(ctx, req)
+	}
+	return d.Daemon.CreateFlock(ctx, req)
 }
